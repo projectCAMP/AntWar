1517abee58cf8f3d6ae12d7fdc762dd99379a083
Don2T4N <oudonbin@gmail.com>
2023-07-17 10:17:50 +0900

    コールバックいらないから消した、コメント追加

--- Assets/Dondon_Scripts/ResourceScript.cs
+++ Assets/Dondon_Scripts/ResourceScript.cs
@@ -1,42 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(ResourceChangeProcess))]
 public class ResourceScript : MonoBehaviour
 {
+    /// <summary>
+    /// 資源の重さ
+    /// </summary>
     [SerializeField]
     private float _mass;
     public float Mass => _mass;
 
+    /// <summary>
+    /// 資源のスコア
+    /// </summary>
     [SerializeField]
     private int _score;
     public int Score => _score;
 
     private Rigidbody2D _rigid;
-    private ResourceChangeProcess _changeProcess;
+    //子オブジェクト判定用
+    private int _previousChildCount;
 
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
-        _changeProcess = GetComponent<ResourceChangeProcess>();
-        _changeProcess.ResourceChanged += CaluculateSpeed;
+        _previousChildCount = transform.childCount;
     }
 
-    public void OnDisable()
+    void Update()
     {
-        gameObject.SetActive(false);
+        //update内で子オブジェクトの数を監視
+        if (transform.childCount != _previousChildCount)
+        {
+            //変化していたら力を計算
+            CaluculateSpeed();
+        }
+        //子オブジェクトの数を合わせる
+        _previousChildCount = transform.childCount;
     }
 
+    /// <summary>
+    /// 子オブジェクト全てを見て、力を計算
+    /// 一番遅いスピードに合わせる
+    /// </summary>
     void CaluculateSpeed()
     {
+        //力
         float power = 0f;
+        //スピード
         float speed = 100f;
+
+        //子オブジェクト全て見る
         foreach (Transform child in transform)
         {
+            //変更予定
             var testSpeed = child.GetComponent<TestSpeed>();
+            //力を足す
             power += testSpeed.Power;
+            //スピードを低いものに合わせる
             if (speed > testSpeed.Speed) speed = testSpeed.Speed;
         }
 
@@ -52,5 +76,13 @@ public class ResourceScript : MonoBehaviour
         {
             _rigid.velocity = Vector2.left * speed;
         }
+    }
+
+    /// <summary>
+    /// 陣地に戻ったら消える
+    /// </summary>
+    public void OnDisable()
+    {
+        gameObject.SetActive(false);
     }
 }
