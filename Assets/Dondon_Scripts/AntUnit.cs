b7b7cdfbdf4d3f0d82f474cb3f88b4aad9eb2b01
Don2T4N <oudonbin@gmail.com>
2023-07-25 11:21:13 +0900

    AntUnitを追加

---
+++ Assets/Dondon_Scripts/AntUnit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntUnit : MonoBehaviour
+{
+  [SerializeField]
+  private int _health;
+  public int Health => _health;
+
+  [SerializeField]
+  private string _name;
+  public string Name => _name;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
