1b36c81d63583f18a4e5e68e418d6ae7e90bb8c7
Don2T4N <oudonbin@gmail.com>
2023-07-25 14:17:17 +0900

    Ant、CSVImporter、AntStatsスクリプトの作成 CSVImporter用のエディター拡張 CSVファイルからのScriptableObjectの自動生成 ResourceスクリプトをAntスクリプト用に調整 テスト用スクリプトの削除

---
+++ Assets/Dondon_Scripts/Ant.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+  /// <summary>
+  /// ステータス参照用
+  /// </summary>
+  [SerializeField]
+  private AntStats _stats;
+  public AntStats Stats => _stats;
+
+  /// <summary>
+  /// 敵ならばtrue
+  /// </summary>
+  [SerializeField]
+  private bool _isHostile;
+  public bool IsHostile => _isHostile;
+
+  /// <summary>
+  /// 現在体力
+  /// </summary>
+  private int _health;
+  public int Health => _health;
+
+  /// <summary>
+  /// 現在スピード
+  /// </summary>
+  private int _speed;
+  public int Speed => _speed;
+
+  /// <summary>
+  /// 現在パワー
+  /// </summary>
+  private int _power;
+  public int Power => _power;
+
+  private void Start()
+  {
+    if (!IsHostile)
+    {
+      _health = Stats.Health;
+      _speed = Stats.Speed;
+      _power = Stats.Power;
+    }
+    else if (IsHostile)
+    {
+      _health = Stats.Health;
+      _speed = Stats.Speed;
+      //敵ならばパワーをマイナスに
+      _power = -Stats.Power;
+    }
+  }
+}
---
+++ Assets/Dondon_Scripts/CSVImporter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create CSV Impoter")]
+public class CSVImporter : ScriptableObject
+{
+  public TextAsset CSV;
+}
