97d304c13f6386c036143006da7efa9f75a6495d
Don2T4N <oudonbin@gmail.com>
2023-11-18 20:16:37 +0900

    ActionButoon内のScene遷移の方法を変更

--- Assets/Tain_Script 1/Buttons/ActionButton.cs
+++ Assets/Tain_Script 1/Buttons/ActionButton.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ActionButton : BasicButton
 {
     //ボタンの役割を表す分岐
-    [SerializeField] Role role;
+    [SerializeField] private Role role;
+
     public enum Role
     {
         unitEditer,
@@ -16,7 +17,8 @@ public class ActionButton : BasicButton
 
     //-----------------------------------------
     //ボタンのアクション内容分岐
-    [SerializeField] ActionSplit actionInput;
+    [SerializeField] private ActionSplit actionInput;
+
     public enum ActionSplit
     {
         none,
@@ -27,17 +29,12 @@ public class ActionButton : BasicButton
     }
 
     //シーン遷移先を入力
-    [SerializeField] SceneName sceneInput;
-    public enum SceneName
-    {
-        none,
-        tainScene,
-        playScene
-    }
+    [SerializeField] private SceneNameEnum sceneInput;
 
     //パネルやポップを受け渡しする
-    [SerializeField] Information.panelJudges panelInput;
-    [SerializeField] Information.popJudges popInput;
+    [SerializeField] private Information.panelJudges panelInput;
+
+    [SerializeField] private Information.popJudges popInput;
 
     //displayにおいてInformationの内容を初期化するために必要
     private void Start()
@@ -55,19 +52,22 @@ public class ActionButton : BasicButton
             case Role.unitEditer:
                 UnitEditing();
                 break;
+
             case Role.sceneMover:
                 SceneMoving();
                 break;
+
             case Role.panelDisplay:
                 PanelChange();
                 break;
+
             case Role.popDisplay:
                 PopChange();
                 break;
         }
     }
 
-    void UnitEditing()
+    private void UnitEditing()
     {
         switch (actionInput)
         {
@@ -75,13 +75,16 @@ public class ActionButton : BasicButton
                 //unitを選択する際に走る処理
                 editer.UnitSelect(this.gameObject);
                 break;
+
             case ActionSplit.unitDecision:
                 //選択が終わりプールを作成する時に走る処理
                 editer.UnitDecision("mine");
                 break;
+
             case ActionSplit.enemyDecision:
                 //敵側のプールを作成する時に走る処理
                 break;
+
             case ActionSplit.unitReset:
                 //選択状態などを全てリセット
                 editer.UnitReset();
@@ -90,25 +93,17 @@ public class ActionButton : BasicButton
     }
 
     //ここだけボタン内で処理を実行する
-    void SceneMoving()
+    private void SceneMoving()
     {
-        switch (sceneInput)
-        {
-            case SceneName.tainScene:
-                SceneLoader.Instance.SceneMove("testmainScene");
-                break;
-            case SceneName.playScene:
-                SceneLoader.Instance.SceneMove("testplayScene");
-                break;
-        }
+        SceneLoader.Instance.SceneMove(sceneInput);
     }
 
-    void PanelChange()
+    private void PanelChange()
     {
         display.PanelDisplay(panelInput);
     }
 
-    void PopChange()
+    private void PopChange()
     {
         display.PopDisplay(popInput);
     }
