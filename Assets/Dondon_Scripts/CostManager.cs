f039bfec86055e6b0652ea3cab2febad703b86bf
Don2T4N <oudonbin@gmail.com>
2023-11-25 19:58:44 +0900

    CostManagerのひな型だけ作成

--- Assets/Dondon_Scripts/CostManager.cs
+++ Assets/Dondon_Scripts/CostManager.cs
@@ -6,6 +6,21 @@ namespace DondonScript
 {
     public class CostManager : MonoBehaviour
     {
-        private PartyScriptableObject partyScriptable;
+        [SerializeField] private int _maxCost = 100;
+        public int MaxCost => _maxCost;
+        private int _currentCost { get; set; }
+
+        private PartyScriptableObject _partyScriptable;
+
+        private void Start()
+        {
+            _currentCost = MaxCost;
+            _partyScriptable = Resources.Load<PartyScriptableObject>("Data/PartyScriptableObject");
+        }
+
+        public void UseCost(int value)
+        {
+            _currentCost = Mathf.Max(0, _currentCost - value);
+        }
     }
 }
